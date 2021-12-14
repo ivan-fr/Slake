30df9a6899773860389ab30b416f6590b09f9db1
BESEVIC Ivan <ivan.besevic_fr@yahoo.com>
2021-12-14 20:17:09 +0100

    update message model

--- src/models/Message.java
+++ src/models/Message.java
@@ -7,14 +7,19 @@ public class Message extends AbstractModel {
     private final Date date;
     private final Integer roomId;
     private final Channel channel;
+    private final User user;
 
-    public Message(String content, Date date, Integer roomId, Channel room) {
+    public Message(String content, Date date, Integer roomId, Channel room, User user) {
         this.content = content;
         this.date = date;
         this.roomId = roomId;
         this.channel = room;
+        this.user = user;
     }
 
+    public Date getDate() {
+        return date;
+    }
     public String getContent() {
         return content;
     }
@@ -25,5 +30,9 @@ public class Message extends AbstractModel {
 
     public Channel getChannel() {
         return channel;
+    }
+
+    public User getUser() {
+        return user;
     }
 }
