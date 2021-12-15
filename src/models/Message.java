ec85cbd05d30cf0669740ca1e999ad73eef3cfc8
BESEVIC Ivan <ivan.besevic_fr@yahoo.com>
2021-12-15 11:56:56 +0100

    update repository and add seed

--- src/models/Message.java
+++ src/models/Message.java
@@ -5,27 +5,22 @@ import java.util.Date;
 public class Message extends AbstractModel {
     private final String content;
     private final Date date;
-    private final Integer roomId;
     private final Channel channel;
     private final User user;
 
-    public Message(String content, Date date, Integer roomId, Channel room, User user) {
+    public Message(String content, Date date, Channel channel, User user) {
         this.content = content;
         this.date = date;
-        this.roomId = roomId;
-        this.channel = room;
+        this.channel = channel;
         this.user = user;
     }
 
     public Date getDate() {
         return date;
     }
+
     public String getContent() {
         return content;
-    }
-
-    public Integer getRoomId() {
-        return roomId;
     }
 
     public Channel getChannel() {
