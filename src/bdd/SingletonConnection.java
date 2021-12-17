d1853183e5925dbce0a5cce2ba3e670a2327a4ea
BESEVIC Ivan <ivan.besevic_fr@yahoo.com>
2021-12-17 15:31:25 +0100

    update

--- src/bdd/SingletonConnection.java
+++ src/bdd/SingletonConnection.java
@@ -1,5 +1,9 @@
 package bdd;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,11 +16,14 @@ public class SingletonConnection {
 
     private static Connection getConnection() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/Slake?user=root&password=Mot de passe");
+            String password = Files.readString(Path.of("./password.txt"), StandardCharsets.UTF_8);
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/Slake?user=root&password=" + password);
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return null;
